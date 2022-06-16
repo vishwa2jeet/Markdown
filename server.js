f363aad2714331e973f31e0e7d8f7904cfa8ccec
vishwa2jeet <vishwak7301@gmail.com>
2022-06-16 12:13:31 +0530

    initial

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const Article =require('./models/article')
+const articleRouter = require('./routes/articles')
+const methodOverride =require('method-override')
+const app = express()
+ 
+
+//mongoose.connect('mongodb://127.0.0.1:27017/blog')
+mongoose.connect('mongodb://localhost:27017/blog')
+
+app.set('view engine','ejs')
+
+app.use(express.urlencoded({extended: false}))
+
+app.use(methodOverride('_method'))
+
+app.get('/',async (req, res) => {
+ const articles = await Article.find ().sort({createdAT:'desc'})
+res.render('articles/index',{articles: articles})
+})
+
+app.use('/articles', articleRouter)
+
+app.listen(5000) 
